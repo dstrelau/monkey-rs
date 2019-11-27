0f8776f91d7c0ce1776ef4da095c8e01bcce3ffb
Dean Strelau <dean@darklang.com>
2019-11-26 21:03:54 -0500

    some more stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,53 @@
-use std::iter::Peekable;
+use std::iter::{Enumerate, Peekable};
 use std::str::Chars;
 
 pub struct Lexer<'a> {
-    chars: Peekable<Chars<'a>>,
+    chars: Peekable<Enumerate<Chars<'a>>>,
 }
 
 impl Lexer<'_> {
     pub fn new<'a>(s: &'a str) -> Lexer<'a> {
         Lexer {
-            chars: s.chars().peekable(),
+            chars: s.chars().enumerate().peekable(),
         }
     }
 
     fn read_identifier(&mut self) -> Token {
-        let mut ident: Vec<char> = vec![];
-        while { self.chars.peek().map_or(false, |c| c.is_alphanumeric()) } {
-            ident.push(self.chars.next().unwrap());
-        }
-        let ident_s = ident.iter().collect::<String>();
-        match ident_s.as_str() {
+        let i = self.read_while(char::is_alphanumeric);
+        match i.as_str() {
             "fn" => Token::Func,
             "let" => Token::Let,
-            _ => Token::Ident(ident_s),
+            "true" => Token::True,
+            "false" => Token::False,
+            _ => Token::Ident(i),
         }
     }
 
+    fn read_int(&mut self) -> Token {
+        let i = self.read_while(char::is_numeric);
+        Token::Int(i)
+    }
+
+    fn read_while<F>(&mut self, f: F) -> String
+    where
+        F: Fn(char) -> bool,
+    {
+        let mut ident: Vec<char> = vec![];
+        while { self.chars.peek().map_or(false, |(_, c)| f(*c)) } {
+            let (_, c) = self.chars.next().unwrap();
+            ident.push(c);
+        }
+        ident.into_iter().collect::<String>()
+    }
+
     fn eat_whitespace(&mut self) {
-        while { self.chars.peek().map_or(false, |c| c.is_whitespace()) } {
+        while { self.chars.peek().map_or(false, |(_, c)| c.is_whitespace()) } {
             self.chars.next();
         }
     }
 
-    fn eat_char(&mut self) {
-        self.chars.next();
-        ()
+    pub fn current_pos(&mut self) -> Option<usize> {
+        self.chars.peek().map(|(pos, _)| *pos)
     }
 }
 
@@ -48,20 +62,68 @@ impl Iterator for Lexer<'_> {
         // another one. explicit destructure here creates copy of char I guess??
         match opt {
             None => None,
-            Some(c) => {
+            Some((idx, c)) => {
                 let tok = match c {
                     'a'..='z' | 'A'..='Z' => self.read_identifier(),
-                    '=' => Token::Assign,
-                    '+' => Token::Plus,
-                    '(' => Token::LParen,
-                    ')' => Token::RParen,
-                    '{' => Token::LBrace,
-                    '}' => Token::RBrace,
-                    ',' => Token::Comma,
-                    ';' => Token::Semi,
-                    _ => Token::Illegal(c.to_string()),
+                    '0'..='9' => self.read_int(),
+                    '=' => {
+                        self.chars.next();
+                        Token::Assign
+                    }
+                    '+' => {
+                        self.chars.next();
+                        Token::Plus
+                    }
+                    '-' => {
+                        self.chars.next();
+                        Token::Minus
+                    }
+                    '*' => {
+                        self.chars.next();
+                        Token::Asterisk
+                    }
+                    '/' => {
+                        self.chars.next();
+                        Token::FSlash
+                    }
+                    '!' => {
+                        self.chars.next();
+                        Token::Bang
+                    }
+                    '<' => {
+                        self.chars.next();
+                        Token::LT
+                    }
+                    '>' => {
+                        self.chars.next();
+                        Token::GT
+                    }
+                    '(' => {
+                        self.chars.next();
+                        Token::LParen
+                    }
+                    ')' => {
+                        self.chars.next();
+                        Token::RParen
+                    }
+                    '{' => {
+                        self.chars.next();
+                        Token::LBrace
+                    }
+                    '}' => {
+                        self.chars.next();
+                        Token::RBrace
+                    }
+                    ',' => {
+                        self.chars.next();
+                        Token::Comma
+                    }
+                    ';' => {
+                        self.chars.next();
+                        Token::Semi
+                    }
+                    _ => Token::Illegal(*idx, c.to_string()),
                 };
-                self.eat_char();
                 Some(tok)
             }
         }
@@ -70,7 +132,7 @@ impl Iterator for Lexer<'_> {
 
 #[derive(Debug, PartialEq)]
 pub enum Token {
-    Illegal(String),
+    Illegal(usize, String),
 
     // Syntax
     LParen,
@@ -82,52 +144,170 @@ pub enum Token {
 
     // Operators
     Plus,
+    Asterisk,
+    FSlash,
+    Bang,
+    GT,
+    LT,
+    Minus,
 
     // Keywords
     Let,
     Func,
     Assign,
+    True,
+    False,
 
     // Values
     Ident(String),
-    Int(i64),
+    Int(String),
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_lexer() {
-        let input = "
-let ten = 10;
-let add = fn(x, y) {
-     x + y;
-};
-let result = add(five, ten);";
-
+    fn expect_tokens(input: &str, tokens: Vec<Token>) {
         let mut lex = Lexer::new(input);
 
-        let expects = [
-            Token::Let,
-            Token::Ident("ten".to_string()),
-            Token::Assign,
-            Token::Int(10),
-            Token::Semi,
-            Token::Let,
-            Token::Ident("add".to_string()),
-            Token::Assign,
-            Token::Func,
-            Token::LParen,
-            Token::Ident("x".to_string()),
-            Token::Comma,
-            Token::Ident("y".to_string()),
-            Token::RParen,
-            Token::Semi,
-        ];
-
-        for ex in expects.iter() {
-            assert_eq!(*ex, lex.next().unwrap())
+        for (i, ex) in tokens.into_iter().enumerate() {
+            let tok = lex.next().unwrap();
+            let pos = lex.current_pos().unwrap_or(0);
+            assert_eq!(
+                ex,
+                tok,
+                "expectation: {}, source char: {},\naround here in source:\n----------\n{}\n----------",
+                i + 1,
+                pos,
+                input
+                    .chars()
+                    .skip(if pos < 5 { 0 } else { pos - 5 })
+                    .take(10)
+                    .collect::<String>()
+                    .escape_default(),
+            )
         }
+        assert_eq!(
+            None,
+            lex.next(),
+            "expected end of input but found more tokens"
+        );
+    }
+
+    #[test]
+    fn test_lexer_some_tokens() {
+        expect_tokens(
+            "a+b; a-b; a*b; a/b;
+            true;
+            false;
+            !a;
+            a > b; b < c;",
+            vec![
+                Token::Ident("a".to_string()),
+                Token::Plus,
+                Token::Ident("b".to_string()),
+                Token::Semi,
+                Token::Ident("a".to_string()),
+                Token::Minus,
+                Token::Ident("b".to_string()),
+                Token::Semi,
+                Token::Ident("a".to_string()),
+                Token::Asterisk,
+                Token::Ident("b".to_string()),
+                Token::Semi,
+                Token::Ident("a".to_string()),
+                Token::FSlash,
+                Token::Ident("b".to_string()),
+                Token::Semi,
+                Token::True,
+                Token::Semi,
+                Token::False,
+                Token::Semi,
+                Token::Bang,
+                Token::Ident("a".to_string()),
+                Token::Semi,
+                Token::Ident("a".to_string()),
+                Token::GT,
+                Token::Ident("b".to_string()),
+                Token::Semi,
+                Token::Ident("b".to_string()),
+                Token::LT,
+                Token::Ident("c".to_string()),
+                Token::Semi,
+            ],
+        )
+    }
+
+    #[test]
+    fn test_lexer_let_int() {
+        expect_tokens(
+            "let five = 5;",
+            vec![
+                Token::Let,
+                Token::Ident("five".to_string()),
+                Token::Assign,
+                Token::Int("5".to_string()),
+                Token::Semi,
+            ],
+        )
+    }
+
+    #[test]
+    fn test_lexer_multiple_lets() {
+        expect_tokens(
+            "let five = 5;
+             let ten = 10;",
+            vec![
+                Token::Let,
+                Token::Ident("five".to_string()),
+                Token::Assign,
+                Token::Int("5".to_string()),
+                Token::Semi,
+                Token::Let,
+                Token::Ident("ten".to_string()),
+                Token::Assign,
+                Token::Int("10".to_string()),
+                Token::Semi,
+            ],
+        )
+    }
+
+    #[test]
+    fn test_lexer_function_and_call() {
+        expect_tokens(
+            "
+    let add = fn(x, y) {
+         x + y;
+    };
+    let result = add(5, 10);",
+            vec![
+                Token::Let,
+                Token::Ident("add".to_string()),
+                Token::Assign,
+                Token::Func,
+                Token::LParen,
+                Token::Ident("x".to_string()),
+                Token::Comma,
+                Token::Ident("y".to_string()),
+                Token::RParen,
+                Token::LBrace,
+                Token::Ident("x".to_string()),
+                Token::Plus,
+                Token::Ident("y".to_string()),
+                Token::Semi,
+                Token::RBrace,
+                Token::Semi,
+                Token::Let,
+                Token::Ident("result".to_string()),
+                Token::Assign,
+                Token::Ident("add".to_string()),
+                Token::LParen,
+                Token::Int("5".to_string()),
+                Token::Comma,
+                Token::Int("10".to_string()),
+                Token::RParen,
+                Token::Semi,
+            ],
+        )
     }
 }
